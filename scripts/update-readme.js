5c694ca4f6b8b88f8c1eaac14055330aba5e073a
李元秋 <yuanqiu.li@wz-inc.com>
2019-05-28 14:45:51 +0800

    update version on pub

---
+++ scripts/update-readme.js
@@ -0,0 +1,22 @@
+const { readFile, writeFile } = require('fs');
+const { argv } = require('process');
+
+const version = argv[2];
+
+readFile('./README.md', (err1, buf1) => {
+  if (err1) {
+    throw err1;
+  }
+  const text = buf1
+    .toString()
+    .replace(
+      /react-final-state@\d+\.\d+\.\d+/g,
+      `react-final-state@${version}`,
+    );
+
+  writeFile('./README.md', text, err2 => {
+    if (err2) {
+      throw err2;
+    }
+  });
+});
