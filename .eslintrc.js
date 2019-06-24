d9da14b18679c46fae3f8de07813da9c334e9433
李元秋 <yuanqiu.li@wz-inc.com>
2019-06-24 20:52:09 +0800

    eslint airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'airbnb-base',
+    'airbnb',
     'plugin:prettier/recommended',
   ],
   parserOptions: {
